92f5bff04b5ea02c06be5b20abfcf08ab720c713
Vineet Abrol <vineetabrol54@gmail.com>
2020-12-03 17:21:05 -0500

    movie app main part done

---
+++ movie-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SearchMovie from "./components/searchMovie"
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie Search </h1>
+      <SearchMovie/>
+    </div>
+  );
+}
+
+export default App;
